3845f088390c888c96983b8f715a345ff6cd8b78
adib-the-noob <mdadib550@gmail.com>
2024-11-16 03:11:50 +0600

    path added

--- app/app.py
+++ app/app.py
@@ -10,6 +10,13 @@ async def home():
         "message": "Hello! This is Home page!"
     }
 
+
+@app.get('/added-path')
+async def added_path():
+    return {
+        "/added-path": "Added through CI-CD pipeline"
+    }
+
 @app.get('/')
 async def root():
     return {
